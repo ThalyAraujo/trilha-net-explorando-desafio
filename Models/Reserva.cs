ebf7eada3039a76867f542f5f03a1b86c414362c
ThalyAraujo <139574005+ThalyAraujo@users.noreply.github.com>
2026-02-11 17:10:46 -0300

    Limpa comentários do código
    Implemented daily rate calculation and discount logic.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -44,14 +44,9 @@ namespace DesafioProjetoHospedagem.Models
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = 0;
             valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados >= 10)
             {
                 valor *= 0.9M;
